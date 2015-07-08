57de4923249fed05e63b3bb2f1da3f3bb7ae3e2c
Joeshmoe523 <joeshmoe523@gmail.com>
2015-07-08 16:34:53 -0700

    Added Home and About Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
 
-
-  resources :items
   devise_for :users
-  root to: "items#index"
+  
+  resources :items
+  resources :users, only: [:show]
+  
+
+  get 'about' => 'welcome#about'
+
+    root to: 'welcome#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
